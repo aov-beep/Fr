5e32e731cfc4e96605f486d82d52d01c394aa170
AleX_hc <aov08022009@gmail.com>
2026-04-04 15:49:31 +0300

    Update hands.py

--- hands.py
+++ hands.py
@@ -2,7 +2,6 @@
 import cv2
 import mediapipe as mp
 
-# Ініціалізація MediaPipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
@@ -13,7 +12,7 @@ hands = mp_hands.Hands(
 
 mp_draw = mp.solutions.drawing_utils
 
-# Камера
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -21,13 +20,11 @@ while True:
     if not success:
         break
 
-    # BGR -> RGB
+
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Обробка кадру
     result = hands.process(img_rgb)
 
-    # Якщо знайдені руки
     if result.multi_hand_landmarks:
         for hand_landmarks in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(
@@ -38,7 +35,7 @@ while True:
 
     cv2.imshow("Hands", img)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # ESC для виходу
+    if cv2.waitKey(1) & 0xFF == 27:  
         break
 
 cap.release()
